54859989c138f154fb553a1d646676e0a448573d
Brent Beulen <47626135+BrentBeulen@users.noreply.github.com>
2022-08-11 20:39:50 +0200

    Update jenkinsApplicatie.sh

--- jenkinsApplicatie.sh
+++ jenkinsApplicatie.sh
@@ -22,7 +22,7 @@ mkdir -p tempdir/https
 cp SportStore.sln tempdir/.
 cp -r src/* tempdir/src/.
 
-cat > tempdir/https/https.config << _EOF_
+cat > tempdir/https/https << _EOF_
 
 [ req ]
 default_bits       = 2048
