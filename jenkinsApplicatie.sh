336b4dbd75533179ba3334dd50a245968d62c124
Brent Beulen <47626135+BrentBeulen@users.noreply.github.com>
2022-08-11 20:53:43 +0200

    Update jenkinsApplicatie.sh

--- jenkinsApplicatie.sh
+++ jenkinsApplicatie.sh
@@ -17,12 +17,12 @@ set -euo pipefail
 
 mkdir -p tempdir
 mkdir -p tempdir/src
-mkdir -p tempdir/https
+mkdir -p /home/vagrant/Applicatie/https
 
 cp SportStore.sln tempdir/.
 cp -r src/* tempdir/src/.
 
-cat > tempdir/https/https.config << _EOF_
+cat > /home/vagrant/Applicatie/https/https.config << _EOF_
 
 [ req ]
 default_bits       = 2048
@@ -46,9 +46,9 @@ extendedKeyUsage    = critical, 1.3.6.1.5.5.7.3.1
 
 _EOF_
 
-openssl req -config tempdir/https/https.config -new -out tempdir/https/csr.pem
-openssl x509 -req -days 365 -extfile tempdir/https/https.config -extensions v3_req -in tempdir/https/csr.pem -signkey key.pem -out tempdir/https/https.crt
-openssl pkcs12 -export -out tempdir/https/https.pfx -inkey key.pem -in tempdir/https/https.crt -password pass:password
+openssl req -config /home/vagrant/Applicatie/https/https.config -new -out /home/vagrant/Applicatie/https/csr.pem
+openssl x509 -req -days 365 -extfile /home/vagrant/Applicatie/https/https.config -extensions v3_req -in /home/vagrant/Applicatie/https/csr.pem -signkey key.pem -out /home/vagrant/Applicatie/https/https.crt
+openssl pkcs12 -export -out /home/vagrant/Applicatie/https/https.pfx -inkey key.pem -in /home/vagrant/Applicatie/https/https.crt -password pass:password
 
 cat > tempdir/Dockerfile << _EOF_
 
@@ -60,7 +60,7 @@ EXPOSE 5000
 ENV ASPNETCORE_URLS=http://+:80
 ENV ASPNETCORE_URLS=https://+:443
 ENV ASPNETCORE_Kestrel__Certificates__Default__Password="password"
-ENV ASPNETCORE_Kestrel__Certificates__Default__Path=/home/vagrant/Applicatie/tempdir/https/https.config
+ENV ASPNETCORE_Kestrel__Certificates__Default__Path=/home/vagrant/Applicatie/https/https.config
 
 # Copy csproj and restore as distinct layers
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
