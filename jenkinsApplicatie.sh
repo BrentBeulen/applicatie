53f444b4267221f94ae44089ade449910df30019
Brent Beulen <47626135+BrentBeulen@users.noreply.github.com>
2022-08-11 22:04:05 +0200

    Update jenkinsApplicatie.sh

--- jenkinsApplicatie.sh
+++ jenkinsApplicatie.sh
@@ -46,19 +46,16 @@ extendedKeyUsage    = critical, 1.3.6.1.5.5.7.3.1
 
 _EOF_
 
-openssl dev-certs https -ep /var/jenkins_home/https/https.pfx -p password
-#openssl req -config /var/jenkins_home/https/https.config -new -out /var/jenkins_home/https/csr.pem
-#openssl x509 -req -days 365 -extfile /var/jenkins_home/https/https.config -extensions v3_req -in /var/jenkins_home/https/csr.pem -signkey key.pem -out /var/jenkins_home/https/https.crt
-#openssl pkcs12 -export -out /var/jenkins_home/https/https.pfx -inkey key.pem -in /var/jenkins_home/https/https.crt -password pass:password
+openssl req -config /var/jenkins_home/https/https.config -new -out /var/jenkins_home/https/csr.pem
+openssl x509 -req -days 365 -extfile /var/jenkins_home/https/https.config -extensions v3_req -in /var/jenkins_home/https/csr.pem -signkey key.pem -out /var/jenkins_home/https/https.crt
+openssl pkcs12 -export -out /var/jenkins_home/https/https.pfx -inkey key.pem -in /var/jenkins_home/https/https.crt -password pass:password
 
 cat > tempdir/Dockerfile << _EOF_
 
 # asp.net SDK versie 5.0
 FROM mcr.microsoft.com/dotnet/aspnet:5.0 As base
 WORKDIR /app
-EXPOSE 80
-EXPOSE 443
-EXPOSE 5000
+EXPOSE 80;443
 ENV ASPNETCORE_URLS="https://+;http://+"
 ENV ASPNETCORE_HTTPS_PORT=443
 ENV ASPNETCORE_Kestrel__Certificates__Default__Password="password"
