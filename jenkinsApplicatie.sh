5f9d427b7b87a3117ce85adab08ae73ddbda2e1b
Brent Beulen <47626135+BrentBeulen@users.noreply.github.com>
2022-08-16 15:52:43 +0200

    Update jenkinsApplicatie.sh

--- jenkinsApplicatie.sh
+++ jenkinsApplicatie.sh
@@ -2,22 +2,6 @@
 # BUILD JOB COMMANDO
 # bash ./jenkinsApplicatie.sh
 
-# PIPELINE CODE
-#node ('TestOmgeving'){
-#    stage('Preparation') {
-#        catchError(buildResult: 'SUCCESS') {
-#            sh 'sudo docker stop SportStoreApp'
-#            sh 'sudo docker rm SportStoreApp'
-#        }
-#    }
-#    stage('Build') {
-#        build 'SportStoreBuild'
-#    }
-#    stage('Deploy') {
-#        sh 'sudo docker run -t -d -p 80:80 -p 443:443 --network vagrant_default -v /home/vagrant/https/:/https/ --name SportStoreApp sportstore'
-#    }
-#}
-
 set -euo pipefail
 
 mkdir -p tempdir
